0142a9cd039ad677c2902ff66ced0235a12036fc
JoniAguero <aguerojonathanivan@gmail.com>
2018-09-29 18:52:58 -0300

    modificaciones

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -2,13 +2,14 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-about',
-  template: `<p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas consectetur malesuada porta. Integer et ipsum vitae
-              erat sodales varius. In malesuada gravida sapien et fermentum. Nulla in lorem aliquam, dapibus nisl vel, vestibulum
-              velit. Phasellus ornare turpis quis orci interdum, et faucibus ipsum fermentum. Aliquam posuere porttitor elit, non
-              placerat sapien porttitor et. Quisque viverra ut nibh sit amet ornare. Curabitur tristique purus diam, in semper nunc
-              rhoncus a. Sed porta et turpis vel aliquet. Morbi quis eleifend massa. Nunc mollis neque at elit mollis vehicula.
-              Mauris sit amet maximus est, a fringilla nulla.
+  template: ` <h2>About Us</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas consectetur malesuada porta. Integer et ipsum vitae
+                erat sodales varius. In malesuada gravida sapien et fermentum. Nulla in lorem aliquam, dapibus nisl vel, vestibulum
+                velit. Phasellus ornare turpis quis orci interdum, et faucibus ipsum fermentum. Aliquam posuere porttitor elit, non
+                placerat sapien porttitor et. Quisque viverra ut nibh sit amet ornare. Curabitur tristique purus diam, in semper nunc
+                rhoncus a. Sed porta et turpis vel aliquet. Morbi quis eleifend massa. Nunc mollis neque at elit mollis vehicula.
+                Mauris sit amet maximus est, a fringilla nulla.
               </p>`,
   styleUrls: ['./about.component.css']
 })
