c692dfd9224c8814ee34267dac7e00ec66b10080
JoniAguero <aguerojonathanivan@gmail.com>
2018-09-29 11:44:12 -0300

    Modififacion template

--- src/app/components/shared/header/header.component.ts
+++ src/app/components/shared/header/header.component.ts
@@ -2,7 +2,12 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html'
+  template: `<div class="jumbotron jumbotron-fluid">
+              <div class="container">
+                <h1 class="display-4">Angular Posts</h1>
+                <p class="lead">With Redux, RxJs, Unit Test, Integration Test, etc. - Author: Jonathan Agüero.</p>
+              </div>
+            </div>`,
 })
 export class HeaderComponent implements OnInit {
 
--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -2,7 +2,20 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
+  template: `<ul class="nav nav-tabs">
+              <li class="nav-item">
+                <a class="nav-link active" href="/hola">Active</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/chau">Link</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled" href="#">Disabled</a>
+              </li>
+            </ul>`,
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
