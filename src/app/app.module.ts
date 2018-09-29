1970acffd984f0edee597b253a0e1d773db5e0ef
JoniAguero <aguerojonathanivan@gmail.com>
2018-09-29 11:41:48 -0300

    Layout Base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/shared/header/header.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
