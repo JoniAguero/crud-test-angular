ec401d9cd5e4ee62114e62ffbdd54b4833d30618
JoniAguero <aguerojonathanivan@gmail.com>
2018-09-30 19:31:19 -0300

    AppModulo Test
    Se configura spec para prueba base

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,22 @@
 import { TestBed, async } from '@angular/core/testing';
+
 import { AppComponent } from './app.component';
-xdescribe('AppComponent', () => {
+import { HeaderComponent } from './components/shared/header/header.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { RouterTestingModule } from '@angular/router/testing';
+
+describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        HeaderComponent,
+        FooterComponent,
+        NavbarComponent,
+      ],
+      imports: [
+        RouterTestingModule.withRoutes([])
       ],
     }).compileComponents();
   }));
@@ -12,16 +24,5 @@ xdescribe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  }));
-  it(`should have as title 'crud-app-test'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('crud-app-test');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to crud-app-test!');
   }));
 });
