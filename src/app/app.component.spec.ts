9d64e5f877ec287e2971216c2bcb7bc3ccc46f46
JoniAguero <aguerojonathanivan@gmail.com>
2018-09-30 19:34:46 -0300

    AppModulo Test
    Se verificar que exista un <router-outlet>

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,6 +5,8 @@ import { HeaderComponent } from './components/shared/header/header.component';
 import { FooterComponent } from './components/shared/footer/footer.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { RouterTestingModule } from '@angular/router/testing';
+import { By } from '@angular/platform-browser';
+import { RouterOutlet } from '@angular/router';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -20,9 +22,17 @@ describe('AppComponent', () => {
       ],
     }).compileComponents();
   }));
+
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
+
+  it('containt router-outlet', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const el = fixture.debugElement.query(By.directive( RouterOutlet ));
+    expect(el).not.toBeNull();
+  }));
+
 });
