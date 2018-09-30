1ca72cc3a2d3e83cdc4242cbd8dd998f50dbe807
JoniAguero <aguerojonathanivan@gmail.com>
2018-09-30 19:56:49 -0300

    Test NavsComponent
    Se verifica que exista un link hacia alguna ruta específica, en este caso /posts

--- src/app/components/shared/navbar/navbar.component.spec.ts
+++ src/app/components/shared/navbar/navbar.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NavbarComponent } from './navbar.component';
+import { By } from '@angular/platform-browser';
+import { RouterLinkWithHref } from '@angular/router';
 
 describe('HeaderComponent', () => {
     let component: NavbarComponent;
@@ -21,6 +23,20 @@ describe('HeaderComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    xit('navs cointain /posts', () => {
+        fixture = TestBed.createComponent(NavbarComponent);
+        const navs = fixture.debugElement.queryAll(By.directive( RouterLinkWithHref ));
+        console.log('navs', navs);
+        let existe = false;
+        for (const nav of navs) {
+            if (nav.attributes['routerLink'] === '/posts') {
+                existe = true;
+                break;
+            }
+        }
+        expect(existe).toBeTruthy();
     });
 
 });
