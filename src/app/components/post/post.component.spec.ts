86d080ca448440931eb8643cc080c9fb2e078dbb
JoniAguero <aguerojonathanivan@gmail.com>
2018-10-06 07:42:15 -0300

    cambios

--- src/app/components/post/post.component.spec.ts
+++ src/app/components/post/post.component.spec.ts
@@ -4,7 +4,22 @@ import { PostComponent } from './post.component';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { PostsService } from '../../shared/services/posts.service';
+import { Subject } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
 
+class FakeActivedRoute {
+
+  private subject = new Subject();
+
+  push(value) {
+    this.subject.next(value);
+  }
+
+  get params() {
+    return this.subject.asObservable();
+  }
+
+}
 
 describe('PostComponent', () => {
   let component: PostComponent;
@@ -28,4 +43,12 @@ describe('PostComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should catch id params', () => {
+    component = fixture.componentInstance;
+    const activatedRoute: FakeActivedRoute = TestBed.get(ActivatedRoute);
+    activatedRoute.push({id: 1});
+    expect(component.id).toBe(1);
+  });
+
 });
